195ce27d06809e652a55ee9b488223620bfdc2ed
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-04-22 16:56:55 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import 'uno.css';
+  import '@fontsource-variable/work-sans/wght.css';
+</script>
+
+<slot />
+
+<svelte:head>
+  <title>EBASHIT</title>
+</svelte:head>
+
+<style>
+  :global(body, html) {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    height: 100%;
+    font-family: 'Work Sans Variable', sans-serif;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<div class="w-screen h-screen grid items-center justify-center bg-red-500">
+  <h1 class="font-bold text-size-9xl text-center strobe-text">EBASHIT</h1>
+</div>
+
+<style>
+  .strobe-text {
+    color: white;
+    animation: strobeEffect 0.15s steps(1, end) infinite; /* 66ms is roughly equivalent to about 15 frames per second */
+  }
+
+  @keyframes strobeEffect {
+    0%,
+    49.99% {
+      color: white;
+    }
+    50%,
+    100% {
+      color: black;
+    }
+  }
+</style>
